6e9fc6de7c89d45074868b31dc0416b39d2c3768
Fabian Keller <login@fabian-keller.de>
2018-09-03 19:25:20 +0200

    Refactor: add lerna.js to create a monorepo

--- packages/core/src/content/entry/asset.entry.ts
+++ packages/core/src/content/entry/asset.entry.ts
@@ -1,31 +1,29 @@
 import {Entry} from './entry';
 import {Observable} from 'rxjs';
 import {Language} from '../../platform/valueobject/language';
+import {entry} from '../metadata/decorators/entry.decorator';
+import {property} from '../metadata/decorators/property.decorator';
 
 /**
  * Represents any file relevant for the content model.
  */
+@entry()
 export class AssetEntry extends Entry {
 
+    @property()
     public content: Observable<string>;
+    @property()
     public mimeType: string;
+    @property()
     public size: number;
+    @property()
     public filename: string;
+    @property()
     public directory: string;
+    @property()
     public title: string;
 
-    constructor(id: string, language: Language, data: any) {
-        super(id, language, data);
-    }
-
-    protected simpleFields(): string[] {
-        return [
-            'content',
-            'mimeType',
-            'size',
-            'filename',
-            'directory',
-            'title'
-        ];
+    constructor(id: string, language: Language) {
+        super(id, language);
     }
 }
